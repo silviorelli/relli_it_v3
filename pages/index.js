cfa5dc3ca6e299c5d5885d0bf449a6a2a23cc7e8
Silvio Relli <silvio@relli.it>
2021-03-16 23:52:46 +0100

    Fetching Contentful data at build time

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,46 @@
 import Head from 'next/head'
+import {createClient} from 'contentful'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default function Home() {
+const space = process.env.CONTENTFUL_SPACE_ID
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
+const client = createClient({
+  space: space,
+  accessToken: accessToken
+})
+
+export async function getHomeData() {
+  try {
+    const allHomepages = await client.getEntries({
+      content_type: 'homepage'
+    })
+    // console.log('------- allHomepages:', allHomepages)
+    return allHomepages.items[0].fields
+  } catch (e) {
+    console.log('------- error:', e)
+  }
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      homeData: await getHomeData()
+    }
+  }
+}
+
+export default function Home({ homeData }) {  
   return (
-    <div className='m-8 text-center'>
-      <p>Hi there!</p>
+    <div>
+      <Head>
+        <title>Silvio Relli</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='m-8 text-center'>
+        <p>Hi there!</p>
+        <p>{homeData.name}</p>
+      </main>
     </div>
   )
 }
