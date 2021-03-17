71f5f3ae2590d36cc9348c5e1f8e197bdd3072f2
Silvio Relli <silvio@relli.it>
2021-03-17 23:38:52 +0100

    Added analytics

--- components/site_head.js
+++ components/site_head.js
@@ -3,21 +3,33 @@ import Head from 'next/head'
 export default function SiteHead({name, profileImage}) {
   return (
     <Head>
-        <title>{name}'s homepage</title>
-        <meta name="description" content={`${name}'s homepage`}/>
-        <meta name="keywords" content={name}/>
-        <meta name="author" content={name}/>
-        <meta name="twitter:card" content={name} />
-        <meta name="twitter:title" content={name} />
-        <meta name="twitter:description" content={`${name}'s homepage`} />
-        <meta name="twitter:image" content={`https:${profileImage.fields.file.url}`} />
-        <meta property="og:site_name" content={`${name}'s homepage`} />
-        <meta property="og:title" content={name} />
-        <meta property="og:url" content="https://www.relli.it" />
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content={`${name}'s homepage`} />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:image" content={`https:${profileImage.fields.file.url}`} />
+      <title>{name}'s homepage</title>
+      <meta name="description" content={`${name}'s homepage`}/>
+      <meta name="keywords" content={name}/>
+      <meta name="author" content={name}/>
+      <meta name="twitter:card" content={name} />
+      <meta name="twitter:title" content={name} />
+      <meta name="twitter:description" content={`${name}'s homepage`} />
+      <meta name="twitter:image" content={`https:${profileImage.fields.file.url}`} />
+      <meta property="og:site_name" content={`${name}'s homepage`} />
+      <meta property="og:title" content={name} />
+      <meta property="og:url" content="https://www.relli.it" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={`${name}'s homepage`} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:image" content={`https:${profileImage.fields.file.url}`} />
+
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=UA-37879690-1`}/>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-37879690-1');
+          `
+        }}
+      />
     </Head>
   )
 }
